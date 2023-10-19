ece80117926a3e76fd77481c0204afc19c7dedfb
cdgritest <cdgri_gcp@gcpcicd-1.asia-northeast3-a.c.gcpcicdtest.internal>
2023-10-19 05:09:08 +0000

    test 231019 140800

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var content = function(req, resp) {
-	resp.end("Hello dwkim on VM2" + "\n");
+	resp.end("Hello dwkim on VM2" + "\n" + "23.10.19-14:08:00 \n");
 	resp.writeHead(200);
 }
 var sv = http.createServer(content);
